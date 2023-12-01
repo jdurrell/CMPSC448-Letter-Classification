c90bfe41e14997edebdade7bc5dec19287c4dc34
jdurrell <john.m.durrell@gmail.com>
2023-11-30 22:33:44 -0500

    Increased model size to achieve 89% test accuracy.

--- CNN.py
+++ CNN.py
@@ -2,26 +2,26 @@ import torch
 import torch.nn 
 import torch.nn.functional
 import torch.optim
+import torch.utils.data
 import torchvision
 
-# Note: this requires reshaping EMNIST images from 28x28 to 32x32
 class CnnModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(1, 6, 5)
-        self.conv2 = torch.nn.Conv2d(6, 16, 5)
-        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = torch.nn.Linear(120, 84)
-        self.fc3 = torch.nn.Linear(84, 47)
+        self.conv1 = torch.nn.Conv2d(1, 50, 5)
+        self.conv2 = torch.nn.Conv2d(50, 240, 5)
+        self.fc1 = torch.nn.Linear(3840, 6000)
+        self.fc2 = torch.nn.Linear(6000, 1000)
+        self.fc3 = torch.nn.Linear(1000, 47)
 
     def forward(self, x):
-        # out.size() = [batch_size, channels, size, size], -1 here means channels*size*size
         out = torch.nn.functional.relu(self.conv1(x))
-        out = torch.nn.functional.avg_pool2d(out, 2)
+        out = torch.nn.functional.max_pool2d(out, 2)
         out = torch.nn.functional.relu(self.conv2(out))
-        out = torch.nn.functional.avg_pool2d(out, 2)
+        out = torch.nn.functional.max_pool2d(out, 2)
 
-        # Convert to linear layer.
+        # Convert to linear layer
+        # out.size() = [batch_size, channels, size, size], -1 here means channels*size*size
         out = out.view(out.size(0), -1)
 
         out = torch.nn.functional.relu(self.fc1(out))
@@ -29,40 +29,37 @@ class CnnModel(torch.nn.Module):
         out = self.fc3(out)
         return out
 
-minibatch_size = 128
+minibatch_size = 32
 num_epochs = 20
 initial_lr = 0.1
 
 def main():
     use_cuda = torch.cuda.is_available()
-    print("Using GPU: ", use_cuda)
+    print(f"Using GPU: {use_cuda}")
 
     # Define the model
     model = CnnModel()
     if use_cuda:
         model = model.cuda()
+    print(f"Number of trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
     # Define the loss function and training algorithm
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=initial_lr)
 
     # Load dataset
-    # MNIST_transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)),
-    #                                                   torchvision.transforms.ToTensor()])
-    # trainset = torchvision.datasets.MNIST(root='./data', train= True, download=True, transform=MNIST_transform)
-    # testset = torchvision.datasets.MNIST(root='./data', train= False, download=True, transform=MNIST_transform)
-
-    EMNIST_transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)), torchvision.transforms.ToTensor()])
+    EMNIST_transform = torchvision.transforms.ToTensor()
     trainset = torchvision.datasets.EMNIST(root="./data", split="balanced", train=True, transform=EMNIST_transform, download=True)
     testset = torchvision.datasets.EMNIST(root="./data", split="balanced", train=False, transform=EMNIST_transform, download=True)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset)) 
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset), shuffle=False)
 
     # Train the model
     for epoch in range(num_epochs):
 
-        optimizer.param_groups[0]["lr"] = compute_learning_rate(epoch)
+        for group in optimizer.param_groups:
+            group["lr"] = compute_learning_rate(epoch)
 
         # Training Step
         for i, (images, labels) in enumerate(trainloader):
@@ -71,10 +68,9 @@ def main():
                 labels = labels.cuda()
 
             # Forward pass to get the loss
-            outputs = model(images) 
+            outputs = model(images)
             loss = criterion(outputs, labels)
 
-            # Backward and compute the gradient
             optimizer.zero_grad()
             loss.backward()  # backpropragation
             optimizer.step() # update the weights/parameters
@@ -85,7 +81,7 @@ def main():
         for i, (images, labels) in enumerate(trainloader):
             if use_cuda:
                 images = images.cuda()
-                labels = labels.cuda()  
+                labels = labels.cuda()
             outputs = model(images)
             p_max, predicted = torch.max(outputs, 1) 
             total += labels.size(0)
@@ -100,7 +96,7 @@ def main():
                 images = images.cuda()
                 labels = labels.cuda()
             outputs = model(images)
-            p_max, predicted = torch.max(outputs, 1) 
+            p_max, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum()
         test_accuracy = float(correct) / total
@@ -109,12 +105,13 @@ def main():
 
 
 def compute_learning_rate(epoch):
-    if epoch < 5:
+    if epoch < 10:
         return initial_lr
-    elif epoch < 10:
+    elif epoch < 15:
         return initial_lr / 10
     else:
         return initial_lr / 100
+
 
 if __name__ == "__main__":
     main()
